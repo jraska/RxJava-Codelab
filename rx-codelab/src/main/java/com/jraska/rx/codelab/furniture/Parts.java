48f743bf24453328e94a3c08eb0e6db4ab34090d
Josef Raska <josef.raska@ocado.com>
2017-03-21 11:35:09 +0000

    Add task 4 error handling

--- rx-codelab/src/main/java/com/jraska/rx/codelab/furniture/Parts.java
+++ rx-codelab/src/main/java/com/jraska/rx/codelab/furniture/Parts.java
@@ -6,8 +6,9 @@ import java.util.List;
 import io.reactivex.Observable;
 
 public final class Parts {
-    public static Screw screw() {
-        return new Screw();
+    public static Observable<Screw> boxOfTenScrews() {
+        return Observable.range(1, 10)
+                .map(value -> new Screw());
     }
 
     public static List<Screw> fiveScrews() {
@@ -18,13 +19,22 @@ public final class Parts {
         return screws;
     }
 
-    public static Observable<Screw> boxOfTenScrews() {
-        return Observable.range(1, 10)
-                .map(value -> new Screw());
+    public static Screw screw() {
+        return new Screw();
     }
 
     public static Observable<Rivet> rivet(Screw screw) {
         return Observable.just(new Rivet());
+    }
+
+    public static Observable<Screw> flakeyScrew() {
+        return Observable.fromCallable(() -> {
+            if (Math.random() > 0.5) {
+                throw new RuntimeException("I'm flakey");
+            }
+
+            return screw();
+        });
     }
 
     private Parts() {
---
+++ rx-codelab/src/test/java/com/jraska/rx/codelab/Task4_ErrorHandling.java
@@ -0,0 +1,47 @@
+package com.jraska.rx.codelab;
+
+import com.jraska.rx.codelab.furniture.Parts;
+import com.jraska.rx.codelab.furniture.Screw;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import io.reactivex.Observable;
+
+public class Task4_ErrorHandling {
+    Observable<Screw> screwsObservable;
+    Observable<Screw> extraScrewsObservable;
+
+    @Before
+    public void before() {
+        screwsObservable = Observable.fromIterable(Parts.fiveScrews())
+                .concatWith(Observable.error(new RuntimeException("Damaged screw!")));
+
+        extraScrewsObservable = Observable.fromIterable(Parts.fiveScrews());
+    }
+
+    @Test
+    public void printErrorMessage() {
+        // TODO: Print all values and incoming error message
+    }
+
+    @Test
+    public void emitCustomItemOnError() {
+        // TODO: When an error happens, emit number artificial screw
+    }
+
+    @Test
+    public void subscribeToExtraObservableOnError() {
+        // TODO: When an error happens, subscribe to extra observable
+    }
+
+    @Test
+    public void retryOnError() {
+        Observable<Screw> flakeyObservable = Parts.flakeyScrew();
+        // TODO: Observable is a bit flakey and often fails, use retry to make it always complete
+
+        for (int i = 0; i < 10; i++) {
+            flakeyObservable.subscribe(System.out::println);
+        }
+    }
+}
