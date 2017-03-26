044b3db8d8a74f1e8d7a75fdf3515b508f85ffdb
Josef Raska <josef.raska@gmail.com>
2017-03-26 15:50:08 +0200

    Setup test indent

--- rx-codelab/src/test/java/com/jraska/rx/codelab/SetupTest.java
+++ rx-codelab/src/test/java/com/jraska/rx/codelab/SetupTest.java
@@ -2,7 +2,6 @@ package com.jraska.rx.codelab;
 
 import com.jraska.rx.codelab.http.HttpModule;
 import com.jraska.rx.codelab.http.RequestInfo;
-
 import org.junit.Test;
 
 import java.io.IOException;
@@ -10,15 +9,15 @@ import java.io.IOException;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class SetupTest {
-    @Test
-    public void everythingSetUp() throws IOException {
-        RequestInfo requestInfo = HttpModule.httpBinApi()
-                .getRequest()
-                .compose(LoggingObserver.transformer())
-                .blockingFirst();
+  @Test
+  public void everythingSetUp() throws IOException {
+    RequestInfo requestInfo = HttpModule.httpBinApi()
+      .getRequest()
+      .compose(LoggingObserver.transformer())
+      .blockingFirst();
 
-        assertThat(requestInfo.url).isNotNull();
-        assertThat(requestInfo.origin).isNotNull();
-        assertThat(requestInfo.headers).isNotEmpty();
-    }
+    assertThat(requestInfo.url).isNotNull();
+    assertThat(requestInfo.origin).isNotNull();
+    assertThat(requestInfo.headers).isNotEmpty();
+  }
 }
