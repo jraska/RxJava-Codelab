8db41979025a1cc246f6a3818292d1e56a64d5d5
Josef Raska <josef.raska@gmail.com>
2017-03-26 15:42:18 +0200

    Task 2 indent

--- rx-codelab/src/test/java/com/jraska/rx/codelab/Task2_TransformationsCuttingTheForest.java
+++ rx-codelab/src/test/java/com/jraska/rx/codelab/Task2_TransformationsCuttingTheForest.java
@@ -1,71 +1,62 @@
 package com.jraska.rx.codelab;
 
-import com.jraska.rx.codelab.forest.BasketOfWood;
-import com.jraska.rx.codelab.forest.Fireplace;
-import com.jraska.rx.codelab.forest.Firewood;
-import com.jraska.rx.codelab.forest.Forest;
-import com.jraska.rx.codelab.forest.Log;
-import com.jraska.rx.codelab.forest.Lumberjack;
-import com.jraska.rx.codelab.forest.Tools;
-import com.jraska.rx.codelab.forest.Tree;
-
+import com.jraska.rx.codelab.forest.*;
+import io.reactivex.Observable;
 import org.junit.Before;
 import org.junit.Test;
 
-import io.reactivex.Observable;
-
 public class Task2_TransformationsCuttingTheForest {
-    Fireplace fireplace;
+  Fireplace fireplace;
 
-    @Before
-    public void before() {
-        fireplace = new Fireplace();
-    }
+  @Before
+  public void before() {
+    fireplace = new Fireplace();
+  }
 
-    @Test
-    public void map_fromOnePieceExactlyToOnePieceOfOtherStuff() {
-        Observable<Tree> treeObservable = Lumberjack.cut(Forest.AMAZON);
+  @Test
+  public void map_fromOnePieceExactlyToOnePieceOfOtherStuff() {
+    Observable<Tree> treeObservable = Lumberjack.cut(Forest.AMAZON);
 
-        // TODO: Transform Observable of Trees to Observable of Firewood. Tools like handSaw and chopping can be useful
+    // TODO: Transform Observable of Trees to Observable of Firewood. Tools like handSaw and chopping can be useful
 
-        Observable<Firewood> woodObservable = null;
-//        fireplace.subscribeForBurn(woodObservable);
-    }
+    Observable<Firewood> woodObservable = null;
+//  fireplace.subscribeForBurn(woodObservable);
+  }
 
-    @Test
-    public void flatMap_chainSawProducesMoreLogs() {
-        // TODO:  Cutting wood by handSaw is not efective, lets use chainSaw now
-        Observable<Firewood> woodObservable = null;
-//        fireplace.subscribeForBurn(woodObservable);
-    }
+  @Test
+  public void flatMap_chainSawProducesMoreLogs() {
+    // TODO:  Cutting wood by handSaw is not efective, lets use chainSaw now
+    Observable<Firewood> woodObservable = null;
+//  fireplace.subscribeForBurn(woodObservable);
+  }
 
-    @Test
-    public void buffer_createBasketsOfWood() {
-        // TODO: We want some bigger fire, lets put firewood into baskets. Buffering int BasketOfWood might be useful
+  @Test
+  public void buffer_createBasketsOfWood() {
+    // TODO: We want some bigger fire, lets put firewood into baskets. Buffering int BasketOfWood might be useful
 
-        Observable<BasketOfWood> basketsObservable = null;
-//        fireplace.subscribeForBasketBurn(basketsObservable);
-    }
+    Observable<BasketOfWood> basketsObservable = null;
+//  fireplace.subscribeForBasketBurn(basketsObservable);
+  }
 
-    @Test
-    public void flatMap_buffer_createBasketsOfWoodWithCuttingMachine() {
-        // TODO: Lets be even more effective and use machineChop tool to get
+  @Test
+  public void flatMap_buffer_createBasketsOfWoodWithCuttingMachine() {
+    // TODO: Lets be even more effective and use machineChop tool to get
 
-        Observable<BasketOfWood> basketsObservable = null;
-//        fireplace.subscribeForBasketBurn(basketsObservable);
-    }
+    Observable<BasketOfWood> basketsObservable = null;
+//  fireplace.subscribeForBasketBurn(basketsObservable);
+  }
 
-    @Test
-    public void debounce_filterTooMuchWoodAtOnce() {
-        Observable<Log> logsObservable = Lumberjack.cut(Forest.AMAZON)
-                .flatMap((tree) -> {
-                    Thread.sleep(tree.treeOrdinal() * 100);
-                    return Tools.chainSaw(tree);
-                });
+  @Test
+  public void debounce_filterTooMuchWoodAtOnce() {
+    Observable<Log> logsObservable = Lumberjack.cut(Forest.AMAZON)
+      .flatMap((tree) -> {
+        Thread.sleep(tree.treeOrdinal() * 100);
+        return Tools.chainSaw(tree);
+      });
 
-        // TODO: Fireplace is able to handle only one firewood per 250 milliseconds, use debounce for inputs to filter too much firewood out
-        Observable<Firewood> woodObservable = logsObservable.map(Tools::chop);
+    // TODO: Fireplace is able to handle only one firewood per 250 milliseconds, use debounce for inputs to filter too much firewood out
+    Observable<Firewood> woodObservable = logsObservable.map(Tools::chop);
 
-//        fireplace.subscribeForBurn(woodObservable);
-    }
+//  fireplace.subscribeForBurn(woodObservable);
+  }
 }
