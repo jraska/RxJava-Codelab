45413c3945a75fd30ccbb98bb6b9c97d06bcde0a
Josef Raska <josef.raska@gmail.com>
2017-03-26 15:39:21 +0200

    Basics task format

--- rx-codelab/src/test/java/com/jraska/rx/codelab/Task1_Basics.java
+++ rx-codelab/src/test/java/com/jraska/rx/codelab/Task1_Basics.java
@@ -3,23 +3,23 @@ package com.jraska.rx.codelab;
 import org.junit.Test;
 
 public class Task1_Basics {
-    @Test
-    public void dummyObservable() {
-        // TODO:  Create Observable with single String value, subscribe to it and print it to console (Observable.just)
-    }
+  @Test
+  public void dummyObservable() {
+    // TODO:  Create Observable with single String value, subscribe to it and print it to console (Observable.just)
+  }
 
-    @Test
-    public void arrayObservable() {
-        // TODO:  Create Observable with ints 1, 2, 3, 4, 5, subscribe to it and print each value to console (Observable.fromArray)
-    }
+  @Test
+  public void arrayObservable() {
+    // TODO:  Create Observable with ints 1, 2, 3, 4, 5, subscribe to it and print each value to console (Observable.fromArray)
+  }
 
-    @Test
-    public void helloOperator() {
-        // TODO:  Create Observable with ints 1 .. 10 subscribe to it and print only odd values (Observable.range, observable.filter)
-    }
+  @Test
+  public void helloOperator() {
+    // TODO:  Create Observable with ints 1 .. 10 subscribe to it and print only odd values (Observable.range, observable.filter)
+  }
 
-    @Test
-    public void receivingError() {
-        // TODO:  Create Observable which emits an error and print the console (Observable.error), subscribe with onError handling
-    }
+  @Test
+  public void receivingError() {
+    // TODO:  Create Observable which emits an error and print the console (Observable.error), subscribe with onError handling
+  }
 }
