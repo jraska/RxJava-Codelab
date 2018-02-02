08b9542881e1610c11ae2bcb59279909131ac715
Emanuil Damyanov <e.damyanov@ocado.com>
2018-02-02 17:41:27 +0200

    Add ascii art to Chair, Table, Sofa, Rivet and Screw

--- rx-codelab/src/main/java/com/jraska/rx/codelab/furniture/Chair.java
+++ rx-codelab/src/main/java/com/jraska/rx/codelab/furniture/Chair.java
@@ -3,6 +3,14 @@ package com.jraska.rx.codelab.furniture;
 public final class Chair {
   @Override
   public String toString() {
-    return "Chair";
+    return
+        "   i______i\n" +
+        "   I______I\n" +
+        "   I      I\n" +
+        "   I______I\n" +
+        "  /      /I\n" +
+        " (______( I\n" +
+        " I I    I I\n" +
+        " I      I";
   }
 }
--- rx-codelab/src/main/java/com/jraska/rx/codelab/furniture/Screw.java
+++ rx-codelab/src/main/java/com/jraska/rx/codelab/furniture/Screw.java
@@ -3,6 +3,25 @@ package com.jraska.rx.codelab.furniture;
 public final class Screw {
   @Override
   public String toString() {
-    return "Screw";
+    return
+        "   ,==\"\"\"\"\"\"\"==,\n" +
+        "    '=._   _.='\n" +
+        "       ('.|\n" +
+        "        |._)\n" +
+        "       ('.|\n" +
+        "        |._)\n" +
+        "       ('.|\n" +
+        "        |._)\n" +
+        "       ('.|\n" +
+        "        |._)\n" +
+        "       ('.|\n" +
+        "        |._)\n" +
+        "       ('.|\n" +
+        "        |._)\n" +
+        "       ('.|\n" +
+        "        |._)\n" +
+        "       ('.|\n" +
+        "        |._)\n" +
+        "       \\_/";
   }
 }
