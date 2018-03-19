17bb13f9f599d0fb4cc93f776864ef3ab12f0ad9
Josef Raska <josef.raska@ocado.com>
2018-03-19 23:57:53 +0000

    Remvoe my name from todos

--- rx-codelab/src/test/java/com/jraska/rx/codelab/Task10_OtherLibrariesInteroperability.java
+++ rx-codelab/src/test/java/com/jraska/rx/codelab/Task10_OtherLibrariesInteroperability.java
@@ -4,17 +4,17 @@ import org.junit.Test;
 
 public class Task10_OtherLibrariesInteroperability {
 
-  // TODO(josef): Uncomment Gradle dependencies for this test
+  // TODO: Uncomment Gradle dependencies for this test
 
   @Test
   public void givenRxJava1and2() {
-    // TODO(josef): Create RxJava 2 observable and convert it to RxJava 1 osbervable and vice versa
-    // TODO(josef): Create RxJava 2 PublishProcessor and convert it to RxJava 1 Subject and back again
+    // TODO: Create RxJava 2 observable and convert it to RxJava 1 osbervable and vice versa
+    // TODO: Create RxJava 2 PublishProcessor and convert it to RxJava 1 Subject and back again
   }
 
   @Test
   public void reactiveStreams_reactor_rxjava() {
-    // TODO(josef): Create RxJava 2 Observable and convert it to Reactor Flux, subscribe
-    // TODO(josef): Create RxJava 2 Single and convert it to Reactor Mono, subscribe
+    // TODO: Create RxJava 2 Observable and convert it to Reactor Flux, subscribe
+    // TODO: Create RxJava 2 Single and convert it to Reactor Mono, subscribe
   }
 }
