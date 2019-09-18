252b474b24c1cc178cf657e70f0999c507fec09a
Josef Raska <6277721+jraska@users.noreply.github.com>
2019-09-18 17:51:50 +0200

    Update Threading example (#27)

---
+++ rx-codelab/src/test/java/com/jraska/rx/codelab/Task5_Threading.java
@@ -0,0 +1,51 @@
+package com.jraska.rx.codelab;
+
+import com.jraska.rx.codelab.http.*;
+import io.reactivex.Observable;
+import io.reactivex.schedulers.Schedulers;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.List;
+
+import static com.jraska.rx.codelab.Utils.sleep;
+
+public class Task5_Threading {
+  private static final String LOGIN = "defunkt"; // One of GitHub founders. <3 GitHub <3
+
+  private GitHubApi gitHubApi = HttpModule.mockedGitHubApi();
+
+  @After
+  public void after() {
+    HttpModule.awaitNetworkRequests();
+  }
+
+  @Test
+  public void zip_subscribeOn_getUserAndHisReposInParallel() {
+    Observable<GitHubUser> userObservable = gitHubApi.getUser(LOGIN);
+    Observable<List<GitHubRepo>> reposObservable = gitHubApi.getRepos(LOGIN);
+
+    // TODO: Get and print Observable<UserWithRepos> whilst running both requests in parallel
+  }
+
+  @Test
+  public void zip_subscribeOn_twoUserAndReposInSerialExplicitly() {
+    Observable<GitHubUser> userObservable = gitHubApi.getUser(LOGIN);
+    Observable<List<GitHubRepo>> reposObservable = gitHubApi.getRepos(LOGIN);
+
+    // TODO: Get and print Observable<UserWithRepos> whilst running both requests in serial order using `Schedulers.single()`
+  }
+
+  @Test
+  public void observeOn_receivingResultsOnDifferentThreads() {
+    Observable<User> userObservable = gitHubApi.getUser(LOGIN).map(GitHubConverter::convert);
+    printWithThreadId("Test thread");
+
+    // TODO: Get user and print him on different threads, use `observeOn`, `doOnNext` and `printWithThreadId` methods
+  }
+
+  private void printWithThreadId(Object object) {
+    System.out.println("Thread id: " + Thread.currentThread().getId() + ", " + object);
+  }
+}
